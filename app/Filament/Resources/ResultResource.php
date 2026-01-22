5367d708652043eb655c4d02465fe9062793e6da
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2026-01-22 15:21:04 +0700

    set to nullabe some table results

---
+++ app/Filament/Resources/ResultResource.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ResultResource\Pages;
+use App\Filament\Resources\ResultResource\RelationManagers;
+use App\Models\Result;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ResultResource extends Resource
+{
+    protected static ?string $model = Result::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('type')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('keyword')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('url')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('target_account')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('capture')
+                    ->maxLength(255),
+        
+                Forms\Components\Select::make('validator_id')
+                    ->relationship('validator', 'name')
+                    ->required(),
+                Forms\Components\Select::make('team_id')
+                    ->relationship('team', 'name')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('validated_at'),
+                Forms\Components\DateTimePicker::make('published_at'),
+                Forms\Components\TextInput::make('status')
+                    ->required(),
+                Forms\Components\TextInput::make('hits')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('keyword')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('url')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('target_account')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('capture')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('domain.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('validator.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('team.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('validated_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('published_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('hits')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListResults::route('/'),
+            'create' => Pages\CreateResult::route('/create'),
+            'edit' => Pages\EditResult::route('/{record}/edit'),
+        ];
+    }
+}
--- database/migrations/2026_01_21_041136_create_results_table.php
+++ database/migrations/2026_01_21_041136_create_results_table.php
@@ -15,18 +15,18 @@ return new class extends Migration
 
         Schema::create('results', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
-            $table->string('keyword');
-            $table->string('url');
-            $table->longText('description');
+            $table->string('type')->nullable();
+            $table->string('keyword')->nullable();
+            $table->string('url')->nullable();
+            $table->longText('description')->nullable();
             $table->string('target_account')->nullable();
             $table->string('capture')->nullable();
-            $table->foreignId('domain_id')->constrained();
-            $table->foreignId('validator_id')->constrained('users');
-            $table->foreignId('team_id')->constrained('users');
+            $table->foreignId('domain_id')->nullable()->constrained();
+            $table->foreignId('validator_id')->nullable()->constrained('users');
+            $table->foreignId('team_id')->nullable()->constrained('users');
             $table->timestamp('validated_at')->nullable();
             $table->timestamp('published_at')->nullable();
-            $table->enum('status',['new','validated','unvalidated']);
+            $table->enum('status',['new','validated','unvalidated'])->default('new');
             $table->integer('hits')->default(0);
             $table->timestamps();
             $table->softDeletes();
