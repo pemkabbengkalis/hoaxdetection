d61b1ec6171e2b56269aa9f21a90718f3ce4320b
meldiadri4n <meldiadri4n@gmail.com>
2026-02-03 16:14:31 +0700

    Update PWA

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -13,10 +13,47 @@ class StatsOverview extends BaseWidget
     {
 
         $result = Result::query()->whereIn('status', ['validated', 'unvalidated'])->get();
-              return [
-            Stat::make('Total URL', $result->count()),
-            Stat::make('Valid', $result->where('status','validated')->count()),
-            Stat::make('Tidak Valid', $result->where('status','unvalidated')->count()),
+        return [
+            Stat::make('Total URL', $result->count())
+                ->description('Jumlah url terdaftar')
+                ->color('white')
+                ->extraAttributes([
+                    'style' => '
+            --fi-stats-card-color: #ffffff;       
+            background-color:rgba(66, 133, 244);
+            color:white;
+            box-shadow:0 3px 8px rgba(176,196,222);
+            border-radius:12px;',
+                    'class' => '!bg-blue-600 !text-danger',
+
+
+                ]),
+            Stat::make('Valid', $result->where('status', 'validated')->count())
+                ->description('Jumlah berita hoaxs terverifikasi')
+                ->color('white')
+                ->extraAttributes([
+                    'style' => '
+            --fi-stats-card-color: #ffffff;       
+            background-color:rgba(52, 168, 83);
+            color:white;
+            box-shadow:0 3px 8px rgba(176,196,222);
+            border-radius:12px;',
+                    'class' => '!bg-blue-600 !text-danger',
+
+                ]),
+            Stat::make('Tidak Valid', $result->where('status', 'unvalidated')->count())
+                ->description('Jumlah tidak valid ')
+                ->color('white')
+                ->extraAttributes([
+                    'style' => '
+            --fi-stats-card-color: #ffffff;       
+            background-color:rgba(234, 67, 53);
+            color:white;
+            box-shadow:0 3px 8px rgba(176,196,222);
+            border-radius:12px;',
+                    'class' => '!bg-blue-600 !text-danger',
+
+                ]),
         ];
     }
 }
