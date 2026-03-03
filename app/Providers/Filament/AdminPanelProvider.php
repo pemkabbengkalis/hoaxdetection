ba4817fcfbf7586b688717b0b1d90c6c64e526bb
meldiadri4n <meldiadri4n@gmail.com>
2026-03-03 09:40:57 +0700

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -93,7 +93,7 @@ class AdminPanelProvider extends PanelProvider
             // ])
 
             ->navigationItems([
-                NavigationItem::make(fn() => app()->getLocale() === 'id' ? 'Dashboard' : 'Dashboard')
+                NavigationItem::make(fn() => app()->getLocale() === 'id' ? 'Dashboard' : 'Dasbor')
                     ->icon('heroicon-s-home')
                     ->url(fn() => route('filament.admin.pages.dashboard'))
                     ->isActiveWhen(fn() => request()->routeIs('filament.admin.pages.dashboard'))
