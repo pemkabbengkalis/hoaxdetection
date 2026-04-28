e676811257b3703d7cf706da5adcdbec86924901
meldiadri4n <meldiadri4n@gmail.com>
2026-04-28 10:07:55 +0700

    up

--- database/migrations/2026_04_20_033709_add_konfirmasi_to_kontens_table.php
+++ database/migrations/2026_04_20_033709_add_konfirmasi_to_kontens_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('kontens', function (Blueprint $table) {
             //
+            $table->text('konfirmasi')->nullable();
         });
     }
 
