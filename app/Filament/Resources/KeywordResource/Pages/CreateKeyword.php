3ebedbc7ef3130ee85abfd5011f2003b03308fd0
meldiadri4n <meldiadri4n@gmail.com>
2026-04-28 10:43:09 +0700

    update notif keyword

--- app/Filament/Resources/KeywordResource/Pages/CreateKeyword.php
+++ app/Filament/Resources/KeywordResource/Pages/CreateKeyword.php
@@ -5,9 +5,29 @@ namespace App\Filament\Resources\KeywordResource\Pages;
 use App\Filament\Resources\KeywordResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+
 
 class CreateKeyword extends CreateRecord
 {
     protected static string $resource = KeywordResource::class;
-    
+
+    //redirect to list after create
+    public function getHeading(): string
+    {
+        return '';
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+    //end of redirect to list after create
+
+    protected function afterSave(): void
+    {
+        Notification::make()
+            ->title('Data berhasil disimpan!')
+            ->success()
+            ->send();
+    }
 }
