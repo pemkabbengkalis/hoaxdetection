35e4a0a18faf40eda3854bf516772d515e955eaf
meldiadri4n <meldiadri4n@gmail.com>
2026-05-26 09:18:25 +0700

    build capcthca cloudflare

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Pages\Auth;
 
 use Filament\Pages\Auth\Login as BaseLogin;
+use Filament\Forms\Form;
+use Filament\Pages\Auth\Login as BaseAuth;
+use Coderflex\FilamentTurnstile\Forms\Components\Turnstile;
 
 class Login extends BaseLogin
 {
@@ -19,5 +22,25 @@ class Login extends BaseLogin
     public function getAuthenticateFormActionLabel(): string
     {
         return 'Masuk';
+    }
+
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getEmailFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getRememberFormComponent(),
+
+                        Turnstile::make('captcha')
+                            ->theme('light')
+                            ->language('id')
+                            ->size('normal'),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
     }
 }
