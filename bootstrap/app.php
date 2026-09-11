7fe6d43334c90b82337dcb9d04556faf4d321f8d
meldiadri4n <meldiadri4n@gmail.com>
2026-09-11 09:13:39 +0700

    update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('livewire:cleanup-temp')
             ->everyMinute()
             ->withoutOverlapping();
+
+        $schedule->command('app:fetch-news')
+            ->everyThreeHours()
+            ->withoutOverlapping();
     })
     ->create()
     ->usePublicPath(
